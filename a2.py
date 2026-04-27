771eea19ed969313bb0e62077175a17a47568c0a
KashafAmjad <shinningstarkshf@gmail.com>
2026-04-27 18:17:19 +0500

    Add files via upload

--- a2.py
+++ a2.py
@@ -1,12 +1,16 @@
-s1 =[2,3,1]
-s2 =['b' 'a' 'c']
-s3 =list(zip(s1,s2))
-print(s3,"\n")
 
-#part2
-stock=['reliance' 'infosys' 'tcs']
-price=[2175,1127,2750]
+import random
+import string
+print('Hello,Welcome to Password generator')
+length=int(input("\nEnter the length of password :"))
 
-new_dict = {stock: prices for stock,
-            prices in zip(stock, prices)}
-print('\n{}'.format(new_dict))
+lower=string.ascii_lowercase
+upper=string.ascii_uppercase
+num=string.digits
+symbol=string.punctuation
+all=lower + upper + num + symbol
+
+temp=random.sample(all,length)
+password="".join(temp)
+
+print(password)
