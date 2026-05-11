5d2266198f59e8d13907fb2f4e183733e8ec2a59
KashafAmjad <shinningstarkshf@gmail.com>
2026-05-11 19:18:01 +0500

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,18 +1,11 @@
-class Person( object ):
-        def __init__(self, name, idnumber):
-         self.name=name
-         self.idnumber=idnumber
-        def display(self):
-                print (self.name)
-                print (self.idnumber)
-class Employee(Person):
-        def __init__(self, name, idnumber,salary,post):
-          self.salary=salary
-          self.post=post
-          Person.__init__(self, name, idnumber)
-print("Employee1 detail:")
-a=Employee("ali", 678929,400000000,"CEO")
-a.display()
-print(a.salary)
-print(a.post)
-print("\n")
+class myClass:
+    __privateVar=27;
+    def__privMeth = (self):
+    print("I'm inside class myClass")
+    def hello(self):
+        print("Private Variable value:",myClass.__privateVar)
+  
+
+obj=myClass()
+obj.hello()
+obj.__privMeth
